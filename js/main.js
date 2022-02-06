996dc9fffa7e564cb970ec3f1cff2f03ac3f263e
hezirel <80560775+hezirel@users.noreply.github.com>
2022-02-06 15:31:09 +0100

    working main search with methods

--- js/main.js
+++ js/main.js
@@ -4,28 +4,33 @@ const dataFetch = async () => await fetch("./js/data.json").then(res => res.json
 let index = document.querySelector("#resultOut");
 let searchInput = document.querySelector(".searchInput");
 
-
-let queryFilterValidator = (request, input) => {
+let queryFilterValidator = (request, data) => {
     if (!request) {
         return true;
     }
-    if (input.name.includes(request)) {
+    let {
+        name,
+        description,
+        ingredients
+    } = data;
+    if (name.includes(request) || description.includes(request) || ingredients.some(e => e.ingredient.includes(request))) {
         return true;
     }
     return false;
 }
 
 const indexFill = async () => {
-    let res = await dataFetch();
+    let resultArray = await dataFetch();
     index.textContent = '';
-    res.forEach((e) => {
+    resultArray.forEach((e) => {
         if (queryFilterValidator(searchInput.value, e)) {
             index.appendChild(cardNode(e))
         }
     })
 }
 
-indexFill();
 searchInput.addEventListener('keyup', (e) => {
-        indexFill();
+    indexFill();
 })
+
+indexFill();

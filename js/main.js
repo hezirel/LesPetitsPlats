8f69ff198c62d827c6f0581c04208533707ffe2f
hezirel <80560775+hezirel@users.noreply.github.com>
2022-02-06 15:13:02 +0100

    semi functionnal main search

--- js/main.js
+++ js/main.js
@@ -1,13 +1,31 @@
 let query = "";
 let filters = [];
 const dataFetch = async () => await fetch("./js/data.json").then(res => res.json());
+let index = document.querySelector("#resultOut");
+let searchInput = document.querySelector(".searchInput");
+
+
+let queryFilterValidator = (request, input) => {
+    if (!request) {
+        return true;
+    }
+    if (input.name.includes(request)) {
+        return true;
+    }
+    return false;
+}
 
 const indexFill = async () => {
     let res = await dataFetch();
-    let index = document.querySelector("#resultOut");
+    index.textContent = '';
     res.forEach((e) => {
-        index.appendChild(cardNode(e))
+        if (queryFilterValidator(searchInput.value, e)) {
+            index.appendChild(cardNode(e))
+        }
     })
 }
 
 indexFill();
+searchInput.addEventListener('keyup', (e) => {
+        indexFill();
+})

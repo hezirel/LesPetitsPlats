1d9ff490be8e297008d9f9945be77888b1c547b5
hezirel <80560775+hezirel@users.noreply.github.com>
2022-02-22 15:37:06 +0100

    formatting

--- study/add.js
+++ study/add.js
@@ -1,4 +1,6 @@
-//#:Function to change for native loop branch
+//#:setup Jest bench env, pursue with native loop branch from there
 let cardAdder = (query, recipe) => {
-	return (recipe.name.includes(query) || recipe.description.includes(query) || recipe.ingredients.includes(query));
+	return (recipe.name.includes(query) ||
+    recipe.description.includes(query) ||
+    recipe.ingredients.includes(query));
 };

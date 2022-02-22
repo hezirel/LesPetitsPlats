11bde7ee924d1d44d3753f924f5c4aa117db3f8b
hezirel <80560775+hezirel@users.noreply.github.com>
2022-02-22 15:02:29 +0100

    blank feed css

--- js/main.js
+++ js/main.js
@@ -83,7 +83,5 @@ let applyQuery = (filter = null) => {
 
 //#:Function to change for native loop branch
 let cardAdder = (query, recipe) => {
-
-
-	//return (recipe.name.includes(query) || recipe.description.includes(query) || recipe.ingredients.includes(query));
+	return (recipe.name.includes(query) || recipe.description.includes(query) || recipe.ingredients.includes(query));
 };

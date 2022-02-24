f476cb3cc832105e25f9d0486279d5910f3ad73a
hezirel <80560775+hezirel@users.noreply.github.com>
2022-02-24 20:57:20 +0100

    native main search, tags case handling

--- study/add.js
+++ study/add.js
@@ -1,6 +1,24 @@
-//#:refactor cardAdder with Object keys
+let strStr = (hay, needle) => {
+	
+	let i = 0;
+
+	for (let char of hay) {
+		if (char === needle[i]) {
+			i++;
+		} else if (i === needle.length) {
+			return true;
+		}
+		else {
+			i = 0;
+		}
+	}
+	return false;
+};
+
 let cardAdder = (query, recipe) => {
+	
 	return Object.keys(recipe).some(e => {
-		return recipe[e].includes(query);
+		return strStr(recipe[e].toLowerCase(), query.toLowerCase());
 	});
+
 };
